d21205a81e326edb74d440c475c397533a468470
freedom8939 <89398368@qq.com>
2024-12-08 20:26:49 +0800

    add: 多套布局支持

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,11 +1,31 @@
 import { RouteRecordRaw } from "vue-router";
 import AccessEnum from "@/access/accessEnum";
+import UserLayout from "@/layouts/UserLayout.vue";
+import UserLoginView from "@/views/user/UserLoginView.vue";
+import UserRegisterView from "@/views/user/UserRegisterView.vue";
 
 export const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "浏览题目",
     component: () => import("../views/HomeView.vue"),
+  },
+  {
+    path: "/user",
+    name: "用户",
+    component: UserLayout,
+    children: [
+      {
+        path: "/user/login",
+        name: "用户登录",
+        component: UserLoginView,
+      },
+      {
+        path: "/user/register",
+        name: "用户注册",
+        component: UserRegisterView,
+      },
+    ],
   },
   {
     path: "/hide",
